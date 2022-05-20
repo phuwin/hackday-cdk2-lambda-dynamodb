b9c70e6f31c5652b2a5c4044958bb499e039f593
Phu Nguyen <ngp130895@gmail.com>
2022-05-20 12:43:28 +0300

    feat: added pipeline to get from main from github

--- lib/hackday-cdk-lambda-dynamodb-stack.ts
+++ lib/hackday-cdk-lambda-dynamodb-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from '@aws-cdk/core';
-
-export class HackdayCdkLambdaDynamodbStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+import {  Stack, StackProps, aws_codepipeline as codepipeline } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+export class HackdayCdkLambdaDynamodbStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('phuwin95/hackday-cdk2-lambda-dynamodb', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
