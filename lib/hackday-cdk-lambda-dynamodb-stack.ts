8a4f81b1f1f0d36b4061c1e4693acc9e987b9582
Phu Nguyen <ngp130895@gmail.com>
2022-05-20 15:42:38 +0300

    feat: added lambda stage

--- lib/hackday-cdk-lambda-dynamodb-stack.ts
+++ lib/hackday-cdk-lambda-dynamodb-stack.ts
@@ -1,6 +1,7 @@
 import {  Stack, StackProps, aws_codepipeline as codepipeline } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import LambdaDynamoDbStage from './lambda-dynamodb';
 export class HackdayCdkLambdaDynamodbStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -12,5 +13,8 @@ export class HackdayCdkLambdaDynamodbStack extends Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+    const myStage = new LambdaDynamoDbStage(this, 'LambdaDynamoDbStack');
+    pipeline.addStage(myStage);
+
   }
 }
---
+++ lib/lambda-dynamodb.ts
@@ -0,0 +1,19 @@
+import { Duration, Stage, StackProps } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+
+export default class CdkStarterStack extends Stage {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new NodejsFunction(this, 'my-function', {
+      memorySize: 256,
+      timeout: Duration.seconds(5),
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'main',
+      entry: join(__dirname, `/../src/my-lambda/index.ts`),
+    });
+  }
+}
