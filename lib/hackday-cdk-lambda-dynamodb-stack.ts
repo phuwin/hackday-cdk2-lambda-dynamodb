279ee3c0331dd9010013153de0391bbe37905629
Phu Nguyen <ngp130895@gmail.com>
2022-05-20 16:03:11 +0300

    fix: added docker for synth

--- lib/hackday-cdk-lambda-dynamodb-stack.ts
+++ lib/hackday-cdk-lambda-dynamodb-stack.ts
@@ -12,7 +12,7 @@ export class HackdayCdkLambdaDynamodbStack extends Stack {
         input: CodePipelineSource.gitHub('phuwin95/hackday-cdk2-lambda-dynamodb', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       }),
-      dockerEnabledForSelfMutation: true,
+      dockerEnabledForSynth: true,
     });
     const myStage = new LambdaDynamoDbStage(this, 'LambdaDynamoDbStack');
     pipeline.addStage(myStage);
